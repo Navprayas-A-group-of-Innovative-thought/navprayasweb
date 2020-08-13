fbcbf88ebb5c6bfeaa9d7b4c15f394d9bea90d98
Kedar Nath <kedar170123023@iitg.ac.in>
2020-08-13 16:04:33 +0530

    fix lastModified bug

--- navprayas/lastModified.py
+++ navprayas/lastModified.py
@@ -1,14 +1,23 @@
 import datetime
+import platform
 
 x = datetime.datetime.now()
-with open("navprayas/lastModified.txt","w") as f:
-    f.write(str(x.strftime("%c")))
+if platform.system() == 'Windows':
+    with open("navprayas\lastModified.txt","w") as f:
+        f.write(str(x.strftime("%c")))
 
-with open("navprayas/lastModified.txt", "r+") as f:
-    y = f.readline()
-    print("reading y as ",y)
+    with open("navprayas\lastModified.txt", "r+") as f:
+        y = f.readline()
+        print("reading y as ",y)
+else :
+    with open("navprayas/lastModified.txt","w") as f:
+        f.write(str(x.strftime("%c")))
 
-class lastMogitdified:
+    with open("navprayas/lastModified.txt", "r+") as f:
+        y = f.readline()
+        print("reading y as ",y)
+
+class lastModified:
     date = y
 
 print(lastModified.date)
