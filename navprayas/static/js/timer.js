a811902aa5d1218b49c1659e8ae4feaa58ec6bbc
Shiva Kumar <kumar.shiva1807@gmail.com>
2020-09-02 18:42:19 +0530

    Mock Test PR

--- navprayas/static/js/timer.js
+++ navprayas/static/js/timer.js
@@ -1,35 +1,21 @@
+var countDownDate = new Date("Sept 12, 2020 23:59:59").getTime();
 
-$(document).ready(function() {
-    $('.countdown').final_countdown({
-    start: '1362139200',
-    end: '1388461320',
-    now: '1387461319',
-    selectors: {
-        value_seconds: '.clock-seconds .val',
-        canvas_seconds: 'canvas_seconds',
-        value_minutes: '.clock-minutes .val',
-        canvas_minutes: 'canvas_minutes',
-        value_hours: '.clock-hours .val',
-        canvas_hours: 'canvas_hours',
-        value_days: '.clock-days .val',
-        canvas_days: 'canvas_days'
-    },
-    seconds: {
-        borderColor: '#7995D5',
-        borderWidth: '6'
-    },
-    minutes: {
-        borderColor: '#ACC742',
-        borderWidth: '6'
-    },
-    hours: {
-        borderColor: '#ECEFCB',
-        borderWidth: '6'
-    },
-    days: {
-        borderColor: '#FF9900',
-        borderWidth: '6'
-    }}, function() {
-    // Finish callback
-    });
-    });
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("timerCnt").innerHTML = days + " D : " + hours + " H : "
+  + minutes + " Min : " + seconds + " Sec ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timerCnt").innerHTML = "EXPIRED";
+  }
+}, 1000);
